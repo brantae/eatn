1f1ea1bc03560171baa413663336bff55dcd95ac
Anslie Brant <brantae@gmail.com>
2023-08-24 23:11:38 -0500

    seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,6 +6,11 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
+# User.delete_all
+# Flair.delete_all
+
+puts "seeding..."
+
 user1 = User.create(name: 'Anslie', username: 'anslieb', password: 'cookie', bio: 'just here to post my girl dinners')
 
 flair1 = Flair.create(name: 'Girl Dinner')
@@ -13,3 +18,5 @@ flair2 = Flair.create(name: '5 Star Cuisine')
 flair3 = Flair.create(name: 'Struggle Meal')
 flair4 = Flair.create(name: 'Munchies')
 flair5 = Flair.create(name: 'Home Chef')
+
+puts "seeded!"
