00b7a582d063b4604789b6e2898c479ddeaa16fc
Anslie Brant <brantae@gmail.com>
2023-08-28 23:03:10 -0500

    sessions and user auth

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,19 @@
+class SessionsController < ApplicationController
+    wrap_parameters format: []
+    def create 
+        puts "Received username: #{params[:username]}"
+        puts "Received password: #{params[:password]}"
+        user = User.find_by(username: params[:username])
+        if user&.authenticate(params[:password]) #if user && user.authenticate is true
+            session[:user_id] = user.id 
+            render json: user, status: :created
+        else 
+            render json: { errors: ["Invalid username or password"]}, status: :unauthorized
+        end
+    end
+    
+    def destroy 
+        session.delete :user_id 
+        head :no_content
+    end
+end
