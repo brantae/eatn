a0dbf33067bdfe1808a1a883d55d4d6cbce14609
Anslie Brant <brantae@gmail.com>
2023-08-29 23:13:45 -0500

    updated seeds and stock images

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,18 +6,35 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
- User.delete_all
-# Flair.delete_all
+Post.delete_all
+User.delete_all
+Flair.delete_all
+
 
 puts "seeding..."
 
-# user1 = User.create(name: 'Anslie', email: 'anslie@gmail.com', username: 'anslieb', password: 'cookie', bio: 'just here to post my girl dinners')
+user1 = User.create(name: 'Anslie B', email: 'anslie@gmail.com', username: 'anslieb', password: 'cookie', bio: 'just here to post my girl dinners')
+user2 = User.create(name: 'Luisa P', email: 'lzp@gmail.com', username: 'lulu', password: 'cake', bio: 'plant-based baddie')
+user3 = User.create(name: 'Meggan Q', email: 'mltq@gmail.com', username: 'nutmeg', password: 'brownie', bio: 'life hack: freeze pesto in ice cube trays')
 
-flair1 = Flair.create(name: 'Girl Dinner')
-flair2 = Flair.create(name: '5 Star Cuisine')
-flair3 = Flair.create(name: 'Struggle Meal')
-flair4 = Flair.create(name: 'Munchies')
-flair5 = Flair.create(name: 'Home Chef')
-flair6 = Flair.create(name: 'Heart Healthy')
+flair1 = Flair.create(name: 'Girl Dinner 🧚')
+flair2 = Flair.create(name: '5 Star Cuisine ⭐️')
+flair3 = Flair.create(name: 'Struggle Meal 🥪')
+flair4 = Flair.create(name: 'Munchies 🍁')
+flair5 = Flair.create(name: 'Home Chef 👩‍🍳')
+flair6 = Flair.create(name: 'Heart Healthy ❤️️')
+flair7 = Flair.create(name: 'Meal Prep 🍴️')
+flair7 = Flair.create(name: 'Plant Based 🌱')
+
+post1 = Post.create(user_id: user1.id, caption: 'test caption')
+post2 = Post.create(user_id: user2.id, caption: 'test caption')
+post3 = Post.create(user_id: user3.id, caption: 'test caption')
+post4 = Post.create(user_id: user1.id, caption: 'test caption')
+
+post1.image.attach(io: File.open(Rails.root.join('db/images/girl-dinner.jpg')), filename: 'girl-dinner.jpg')
+post2.image.attach(io: File.open(Rails.root.join('db/images/meal-prep.jpg')), filename: 'meal-prep.jpg')
+post3.image.attach(io: File.open(Rails.root.join('db/images/munchies.jpg')), filename: 'munchies.jpg')
+post4.image.attach(io: File.open(Rails.root.join('db/images/struggle-meal.jpg')), filename: 'struggle-meal.jpg')
+
 
 puts "seeded!"
