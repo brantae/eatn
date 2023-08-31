a5d129b55e6b10d80d940b620a7e71e6b9639e39
Anslie Brant <brantae@gmail.com>
2023-08-31 01:45:44 -0500

    fixed id not being added

--- app/serializers/post_serializer.rb
+++ app/serializers/post_serializer.rb
@@ -1,6 +1,6 @@
 class PostSerializer < ActiveModel::Serializer
   include Rails.application.routes.url_helpers
-  attributes :caption, :user_id, :image, :user_name
+  attributes :id, :caption, :user_id, :image, :user_name
 
   def image
     rails_blob_path(object.image, only_path: true) if object.image.attached?
