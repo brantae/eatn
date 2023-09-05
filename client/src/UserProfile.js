6bbe681cf53fd92c13b93addc925a7441afdde41
Anslie Brant <brantae@gmail.com>
2023-09-04 23:05:56 -0500

    created user profile page

--- client/src/UserProfile.js
+++ client/src/UserProfile.js
@@ -1,13 +1,42 @@
+import React, { useContext } from 'react';
+import { Container, Header, Icon, Segment } from 'semantic-ui-react';
+import { UserContext } from './context/UserContext'
 
+function UserProfile() {
+  const { currentUser } = useContext(UserContext);
 
-export default function UserProfile() {
+  if (!currentUser) {
+    return (
+      <Container text style={{ marginTop: '2em' }}>
+        <Header as="h2" icon textAlign="center">
+          <Icon name="user circle" circular />
+          <Header.Content>user profile</Header.Content>
+        </Header>
+        <Segment>
+          <p>please log in to view your profile</p>
+        </Segment>
+      </Container>
+    );
+  }
+
   return (
-    <div>
-        <h2>User Profile Page</h2>
-        <p>Show logged in user in nav bar</p>
-        <p>show account details</p>
-        <p>name, user, bio</p>
-        
-    </div>
-  )
+    <Container text style={{ marginTop: '2em' }}>
+      <Header as="h2" icon textAlign="center">
+        <Icon name="user circle" circular />
+        <Header.Content>User Profile</Header.Content>
+      </Header>
+      <Segment.Group>
+        <Segment>
+          <Header as="h3">username</Header>
+          <p>{currentUser.username}</p>
+        </Segment>
+        <Segment>
+          <Header as="h3">email</Header>
+          <p>{currentUser.email}</p>
+        </Segment>
+      </Segment.Group>
+    </Container>
+  );
 }
+
+export default UserProfile
